392c2ca411d9b8794e0d872a32ab4e6fae329a8d
Yakup Geyik <yakuppgeyik@gmail.com>
2025-10-31 23:34:23 +0300

    ft: remove mock products

--- apps/gratia-ui/src/constants/api.ts
+++ apps/gratia-ui/src/constants/api.ts
@@ -1,3 +1,2 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://gratia-api.onrender.com";
+export const API_BASE_URL = process.env.API_URL || "";
 export const API_BASE_PATH = `${API_BASE_URL}/api`;
