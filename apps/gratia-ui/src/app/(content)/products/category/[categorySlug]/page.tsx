c4c09df0711172d9be6caf91d924c730e2a80157
Yakup Geyik <yakuppgeyik@gmail.com>
2025-11-16 19:04:56 +0300

    remove search params from products category

--- apps/gratia-ui/src/app/(content)/products/category/[categorySlug]/page.tsx
+++ apps/gratia-ui/src/app/(content)/products/category/[categorySlug]/page.tsx
@@ -59,18 +59,15 @@ export async function generateMetadata({
 
 export default async function CategoryProductsPage({
   params,
-  searchParams,
 }: CategoryProductsPageProps) {
   const { categorySlug } = await params;
-  const { page } = await searchParams;
 
-  const pageNumber = page ? parseInt(page, 10) : 1;
-  const validPage = isNaN(pageNumber) || pageNumber < 1 ? 1 : pageNumber;
+  /* const pageNumber = page ? parseInt(page, 10) : 1;
+  const validPage = isNaN(pageNumber) || pageNumber < 1 ? 1 : pageNumber; */
 
   try {
     const { data } = await getProducts({
       categorySlug,
-      page: validPage,
     });
 
     return (
