473994cdaf8527e0fe723f129960e45bfa824eb5
Yakup Geyik <yakuppgeyik@gmail.com>
2025-12-26 09:50:15 +0300

    add loading for category and collection pages

---
+++ apps/gratia-ui/src/app/(main)/(content)/products/category/[categorySlug]/loading.tsx
@@ -0,0 +1,5 @@
+import ProductsLoading from "../../loading";
+
+export default function CategoryProductsLoading() {
+  return <ProductsLoading />;
+}
---
+++ apps/gratia-ui/src/app/(main)/(content)/products/collection/[collectionSlug]/loading.tsx
@@ -0,0 +1,5 @@
+import ProductsLoading from "../../loading";
+
+export default function CollectionProductsLoading() {
+  return <ProductsLoading />;
+}
