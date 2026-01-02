2c7f363bd8a0651bc5098ceef2d78d8396871cd2
Yakup Geyik <yakuppgeyik@gmail.com>
2026-01-02 13:52:54 +0300

    Render home page again

--- apps/gratia-ui/src/app/(main)/page.tsx
+++ apps/gratia-ui/src/app/(main)/page.tsx
@@ -1,12 +1,21 @@
-// import { FeaturedProductsSection } from "@/components/featured-products";
-// import { HeroSection } from "@/components/hero";
+import dynamic from "next/dynamic";
+
+const HeroSection = dynamic(() => import("@/components/hero/HeroSection"), {
+  ssr: true,
+});
+const FeaturedProductsSection = dynamic(
+  () => import("@/components/featured-products/FeaturedProductsSection"),
+  {
+    ssr: true,
+  }
+);
 
 export default async function Home() {
   return (
     <>
       <div>Hello World</div>
-      {/* <HeroSection /> */}
-      {/* <FeaturedProductsSection /> */}
+      <HeroSection />
+      <FeaturedProductsSection />
     </>
   );
 }
